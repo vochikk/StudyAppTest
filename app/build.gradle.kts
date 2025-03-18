2475151923c45578290da841ed7b82b30178552e
vochik <vochik@live.ru>
2025-03-19 01:52:44 +0300

    onboarding fragment

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,9 @@
+import org.jetbrains.kotlin.gradle.internal.Kapt3GradleSubplugin.Companion.findKaptConfiguration
+
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.kotlin.kapt)
 }
 
 android {
@@ -45,4 +48,28 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
+    // navigation
+    implementation(libs.androidX.navigation.fragment)
+    implementation(libs.androidX.navigation.ui)
+
+    // fragment
+    implementation(libs.androidX.fragment)
+
+    // database
+    implementation(libs.room.common)
+    implementation(libs.room.runtime)
+    implementation(libs.room.ktx)
+    kapt(libs.room.compiler)
+
+    // dependency Injection
+    implementation(libs.koin)
+
+    // network
+    implementation(libs.retrofit2.retrofit)
+    implementation(libs.retrofit2.converter.gson)
+
+    // image
+    implementation(libs.glide)
+    kapt(libs.glide.compiler)
 }
